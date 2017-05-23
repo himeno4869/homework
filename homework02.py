2afb83f76888dd3738e6f90634de81fda478c1ee
8176398389 <8176398389@pc301015m.ecc.u-tokyo.ac.jp>
2017-05-23 09:37:16 +0900

    modify

--- homework02.py
+++ homework02.py
@@ -3,15 +3,14 @@
 def is_power(a,b,count):
     if a%b == 0:
         count+=1
-        print(count)
-        is_power(a/b, b, count)
+        is_power(a//b, b, count)
     else:
-        if count > 0:
+        if a==1:
             print("{0} is the {1}th power of {2}".format(b**count, count, b))
             
         else:
-            print("{0} is not a power of {1}".format(a,b))
+            print("{0} is not a power of {1}".format(a*(b**count),b))
 
 
 if __name__ == "__main__":
-    is_power(5,2,0)
+    is_power(6,2,0)
