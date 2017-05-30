7a0122f712bc807a0f78d3589314ea98ade0f91f
8176398389 <8176398389@pc301015m.ecc.u-tokyo.ac.jp>
2017-05-30 10:09:00 +0900

    hogehoge

--- homework03.py
+++ homework03.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def x2_calcurate(x, x1):
+def x2_calculate(x, x1):
     '''微分方程式を元にして加速度の値を計算する関数'''
     return -2*gamma*x1 - omega_2*x
 #以下は微分方程式の初期値
@@ -21,12 +21,12 @@ x1_array = np.zeros_like(t_array)
 x2_array = np.zeros_like(t_array)
 x_array[0] = x
 x1_array[0] = x1
-x2_array[0] = x2_calcurate(x, x1)
+x2_array[0] = x2_calculate(x, x1)
 
 for i in np.arange(len(t_array)-1):
     x_array[i+1] = x_array[i] + x1_array[i]*dt #dt後のxの値の計算
     x1_array[i+1] = x1_array[i] + x2_array[i]*dt #dt後のvの値の計算
-    x2_array[i+1] = x2_calcurate(x_array[i+1], x1_array[i+1]) #dt後の加速度の値の更新
+    x2_array[i+1] = x2_calculate(x_array[i+1], x1_array[i+1]) #dt後の加速度の値の更新
     
 #以下は計算結果表示
 plt.subplot(3, 1, 1)
