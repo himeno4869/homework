d4027e697d92b01cabb7927d4454083cf015116f
himeno4869 <yu.himeno36@gmail.com>
2017-07-29 12:16:02 +0900

    'sdcv'

--- jukovsky.py
+++ jukovsky.py
@@ -1,43 +1,173 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+
 import matplotlib.pyplot as plt
 
-def potential(z, a, v0, gamma):
-    
-    return v0*(z+(a**2)/z) + np.complex(0, (gamma/(2*np.pi))*np.log(z))
 
-if __name__ == "__main__":
-    a = 0.7
+
+def potential(z, a, v0, gamma):
+
+    
+
+    return v0*(z+(a**2)/z) + gamma*np.log(z)
+
+def main():
+    
+    a = 0.6
+
     v0 = 1.0
-    gamma = 5.0
-    c = 0.6
-    x = np.arange(-10.1, 10.1, 0.1)
-    y = np.arange(-10.1, 10.1, 0.1)
-    X, Y = np.meshgrid(x,y)
-    '''z = np.zeros([202, 202], dtype=np.complex)
-    for i in range(202):
-        for j in range(202):
-            z[j][i] = np.complex(x[i], y[j])
-            z[101][101] = np.complex(0.1, 0.1)
-    x = (z+(c**2)/z).real
-    y = (z+(c**2)/z).imag
-    '''
-    zz = np.zeros([202, 202], dtype=np.complex)
-    for i in range(202):
-        for j in range(202):
-            zz[j][i] = potential(np.complex(x[i], y[j]), a, v0, gamma)
-            if abs(zz[j][i]) > 100:
-                zz[j][i] = 0
-            if x[i]**2 + y[j]**2 < a**2:
-                zz[j][i] = 0
-    z = np.zeros([202, 202], dtype=np.complex)
-    for i in range(202):
-        for j in range(202):
-            z[j][i] = np.complex(x[i], y[j])
-            z[101][101] = np.complex(0.1, 0.1)
-    x = (z+(c**2)/z).real
-    y = (z+(c**2)/z).imag
+
+    c = 0.5
+
+    alfa = 5.0*np.pi/180
+
+    beta = 20*np.pi/180
+
+    gamma = 4*np.pi*v0*a*np.sin(alfa+beta)
+
+    cgama = np.complex(0, gamma/(2*np.pi))
+
+    dr = 0.05
+
+    thetar = 360
+
+    xmin = -5
+
+    xmax = 5
+
+    rnge = xmax - xmin
+
+    rrnge = int(rnge / (2*dr))
+
+    dtheta = 2*np.pi / thetar
+
+    center = np.complex(c, 0) + a*np.exp(np.complex(0, np.pi-beta))
+
+    x = np.zeros([rrnge, thetar], dtype=np.complex)
+
+    y = np.zeros([rrnge, thetar], dtype=np.complex)
+
+    for i in range(rrnge):
+
+        for k in range(thetar):
+
+            x[i][k] = ((a+dr*i) * np.exp(np.complex(0, dtheta*k-beta))).real
+
+            y[i][k] = ((a+dr*i) * np.exp(np.complex(0, dtheta*k-beta))).imag
+
+    
+
+    zz = np.zeros([rrnge, thetar], dtype=np.complex)
+
+    for i in range(rrnge):
+
+        for k in range(thetar):
+
+            zz[i][k] = potential(np.complex(x[i][k], y[i][k])*np.exp(np.complex(0, -alfa)), a, v0, cgama)
+
+           
+
+    z = np.zeros([rrnge, thetar], dtype=np.complex)
+
+
+
+    for i in range(rrnge):
+
+        for k in range(thetar):
+
+            z[i][k] = np.complex(x[i][k], y[i][k]) + center
+
+
+
+    X = (z+(c**2)/z).real
+
+    Y = (z+(c**2)/z).imag
+
+
+    z2 = np.zeros([rrnge, thetar], dtype=np.complex)
+
+    z1 = np.zeros([rrnge, thetar], dtype=np.complex)
+
+    
+
+    for i in range(rrnge):
+
+        for k in range(thetar):
+
+            z2[i][k] = np.complex(x[i][k], y[i][k]) + center
+
+
+
+    for i in range(rrnge):
+
+        for k in range(thetar):
+
+            z1[i][k] = (z2[i][k]-center)*np.exp(np.complex(0, -alfa))
+
+
+
+    f = np.exp(np.complex(0, -alfa))*(v0*(1-(a**2)/(z1**2))+cgama/z1)/(1-(c**2)/(z2**2))
+
+    f1 = 1-(f.real**2+f.imag**2)/v0**2
+
+           
+
+    for i in range(rrnge):
+
+        for k in range(thetar):
+
+            if abs(f1[i][k]) > 4:
+
+                f1[i][k] = 0
+
+                  
+
+    cxp = 0
+
+    cyp = 0
+
+    for i in range(thetar-1):
+
+        dxw = X[0][i+1] - X[0][i]
+
+        dyw = Y[0][i+1] - Y[0][i]
+
+        dnx = dyw
+
+        dny = -dxw
+
+        cpm = (f1[0][i+1]+f1[0][i])/2
+
+        cxp = cxp - cpm*dnx
+
+        cyp = cyp - cpm*dny
+
+    
+
+    cxp = cxp/(4*c)
+
+    cyp = cyp/(4*c)
+
+    cdp = cxp*np.cos(alfa) + cyp*np.sin(alfa)
+
+    clp = cyp*np.cos(alfa) - cxp*np.sin(alfa)
+
+    
+
+    print('Cd = ' + str(cdp))
+
+    print('Cl = ' + str(clp))
+
+    
+
+    
+
     plt.figure(figsize=(5,4))
-    plt.contour(x, y, zz.imag, 100)
+
+    plt.contour(X, Y, f1, 100)
+
     plt.colorbar()
+    
+if __name__ == "__main__":
+    main()
