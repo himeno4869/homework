d7aa4e4d04231c7af6dcdef63345219ef1d30dfc
himeno4869 <yu.himeno36@gmail.com>
2017-07-29 11:36:45 +0900

    'wer'

--- jukovskyr.py
+++ jukovskyr.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     beta = 20*np.pi/180
     gamma = 4*np.pi*v0*a*np.sin(alfa+beta)
     cgama = np.complex(0, gamma/(2*np.pi))
-    dr = 0.5
+    dr = 0.05
     thetar = 360
     xmin = -5
     xmax = 5
@@ -43,7 +43,60 @@ if __name__ == "__main__":
 
     X = (z+(c**2)/z).real
     Y = (z+(c**2)/z).imag
+    
+    xx = np.zeros(thetar, dtype=np.complex)
+    yy = np.zeros(thetar, dtype=np.complex)
+    
+    for i in range(thetar):
+        xx[i] = (a*np.exp(np.complex(0, dtheta*i))).real
+        yy[i] = (a*np.exp(np.complex(0, dtheta*i))).imag
+    
+    for i in range(thetar):
+        zzz = np.complex(xx[i], yy[i])
+        
+    XX = (zzz+(c**2)/zzz).real
+    YY = (zzz+(c**2)/zzz).imag
+    
+    
+    z2 = np.zeros([rrnge, thetar], dtype=np.complex)
+    z1 = np.zeros([rrnge, thetar], dtype=np.complex)
+    
+    for i in range(rrnge):
+        for k in range(thetar):
+            z2[i][k] = np.complex(x[i][k], y[i][k]) + center
 
+    for i in range(rrnge):
+        for k in range(thetar):
+            z1[i][k] = (z2[i][k]-center)*np.exp(np.complex(0, -alfa))
+
+    f = np.exp(np.complex(0, -alfa))*(v0*(1-(a**2)/(z1**2))+cgama/z1)/(1-(c**2)/(z2**2))
+    f1 = 1-(f.real**2+f.imag**2)/v0**2
+           
+    for i in range(rrnge):
+        for k in range(thetar):
+            if abs(f1[i][k]) > 4:
+                f1[i][k] = 0
+                  
+    cxp = 0
+    cyp = 0
+    for i in range(thetar-1):
+        dxw = X[0][i+1] - X[0][i]
+        dyw = Y[0][i+1] - Y[0][i]
+        dnx = dyw
+        dny = -dxw
+        cpm = (f1[0][i+1]+f1[0][i])/2
+        cxp = cxp - cpm*dnx
+        cyp = cyp - cpm*dny
+    
+    cxp = cxp/(4*c)
+    cyp = cyp/(4*c)
+    cdp = cxp*np.cos(alfa) + cyp*np.sin(alfa)
+    clp = cyp*np.cos(alfa) - cxp*np.sin(alfa)
+    
+    print('Cd = ' + str(cdp))
+    print('Cl = ' + str(clp))
+    
+    
     plt.figure(figsize=(5,4))
-    plt.contour(X, Y, zz.imag, 40)
+    plt.contour(X, Y, f1, 100)
     plt.colorbar()
